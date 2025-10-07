1eabd6ab984ff032a9d3f183c2ad1b2f5824d50e
netakiryosuke <sigoryosuke@gmail.com>
2025-10-08 00:21:12 +0900

    質問をページネーション

--- src/components/QuestionPage.jsx
+++ src/components/QuestionPage.jsx
@@ -1,17 +1,31 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import Question from "./Question";
 import { useState } from "react";
 
 export default function QuestionPage() {
     const questions = [
         "あなたは自分の意見をはっきり言う方だと思いますか？",
-        "あなたは新しいことに挑戦するのが好きですか？",
-        "あなたは計画を立てて行動するタイプですか？",
-        "あなたは人と協力して仕事をするのが得意ですか？",
-        "あなたはストレスを感じたときにリラックスする方法を知っていますか？"
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "11",
+        "12",
+        "13",
+        "14",
+        "15"
     ];
+    
 
     const [answers, setAnswers] = useState(Array(questions.length).fill(null));
+    const [currentPage, setCurrentPage] = useState(0);
+
+    const displayingQuestions = questions.slice(0 + (currentPage*5), 5 + (currentPage*5));
 
     const handleSelect = (answer, questionIndex) => {
         const newAnswers = [...answers];
@@ -19,11 +33,19 @@ export default function QuestionPage() {
         setAnswers(newAnswers);
     }
 
+    const handleNextClick = () => {
+        setCurrentPage(currentPage + 1);
+    }
+
     return (
-        <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", gap: 6}}>
-            {questions.map((question, index) => (
-                <Question key={index} question={question} selectedIndex={answers[index]} onSelect={(answer) => handleSelect(answer, index)} />
-            ))}
+        <Box>
+            <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", gap: 6}}>
+                {displayingQuestions.map((question, index) => (
+                    <Question key={index} question={question} selectedIndex={answers[index]} onSelect={(answer) => handleSelect(answer, index)} />
+                ))}
+            </Box>
+            <Button onClick={handleNextClick}>次へ→</Button>
         </Box>
+        
     )
 }
