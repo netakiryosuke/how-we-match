cfed1580fdb01bc532fb2eb08ebfc24919a04faa
netakiryosuke <sigoryosuke@gmail.com>
2025-10-12 22:16:17 +0900

    アニメーション付きのルーティングを実装

--- src/components/AnimatedRoutes.jsx
+++ src/components/AnimatedRoutes.jsx
@@ -1,13 +1,42 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import QuestionPage from "./QuestionPage";
 import Result from "./Result";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function AnimatedRoutes() {
-    
+    const location = useLocation();
+
     return (
-        <Routes>
-            <Route path="/" element={<QuestionPage />} />
-            <Route path="/result" element={<Result />} />
-        </Routes>
+        <AnimatePresence mode="wait">
+            <Routes location={location} key={location.pathname}>
+                <Route
+                    path="/"
+                    element={
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ duration: 0.5 }}
+                        >
+                            <QuestionPage />
+                        </motion.div>
+                    }
+                />
+                <Route
+                    path="/result"
+                    element={
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ duration: 0.5 }}
+                        >
+                            <Result />
+                        </motion.div>
+                    }
+                />
+            </Routes>
+        </AnimatePresence>
+
     );
 }
