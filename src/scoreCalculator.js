6bd6aedb69928496f0c119024e22e29a7c34b8d7
netakiryosuke <sigoryosuke@gmail.com>
2025-10-12 21:45:56 +0900

    計算ロジックの実装

--- src/scoreCalculator.js
+++ src/scoreCalculator.js
@@ -1,8 +1,8 @@
 export default function calculateScore(answers) {
-    const score = answers.reduce((sum, answer) => {
-        if (answer === null) return sum;
-        
-        return sum + answer - 3
-    }, 0);
-    return score;
+    const weights = [1, -2, -0.7, 0, 1.2, -1, -0.5, -2, -0.2, 0.8, -1.3, 1.2, 1, 1, 0.4, -1, 0.3, -1, 1, -1];
+
+    const normalizedAnswers = answers.map((answer, i) => (answer - 3) * weights[i]);
+    const score = normalizedAnswers.reduce((sum, normalizedAnswer) => sum + normalizedAnswer, 0);
+
+    return Math.ceil(((score + 55.8) / 111.6 ) * 100);
 }
