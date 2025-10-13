01da532662acb177397f55dd422539a35422eb1a
Ryosuke Osawa <sigoryosuke@gmail.com>
2025-10-13 16:40:55 +0900

    Add gh-pages config (#15)
    * chore: gh-pagesのインポート
    
    * gh-pagesのデプロイスクリプト設定を追加
    
    * vite.config.jsの修正
    
    * HashRouterを使用するよう修正
    
    * 末尾改行

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './App.css';
 import AnimatedRoutes from './components/AnimatedRoutes';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AnimatedRoutes />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
