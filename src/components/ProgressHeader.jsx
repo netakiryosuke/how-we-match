3af54a73b00095f492208509d255118797a8fa99
netakiryosuke <sigoryosuke@gmail.com>
2025-10-13 18:39:57 +0900

    前の質問に戻るボタンのログ出力に遊び心を追加

--- src/components/ProgressHeader.jsx
+++ src/components/ProgressHeader.jsx
@@ -18,7 +18,7 @@ export default function ProgressHeader({ isPage1, answeredQuestion, totalQuestio
                 pl: "4vw",
             }}>
             <Tooltip title="最初にした回答が一番自分を表していると思いますよ。" arrow>
-                <Button disabled={isPage1}>←前の質問に戻る</Button>
+                <Button onClick={() => console.log("If you wanna go back, just reload the page. Or... you could make that feature yourself and send me a PR.")} disabled={isPage1}>←前の質問に戻る</Button>
             </Tooltip>
             <LinearProgress variant="determinate" value={progress} sx={{ width: "80vw", height: 8, borderRadius: 5 }} />
         </Paper>
