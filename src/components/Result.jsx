3c537076043a3dca631ddf15cfbf9ba5e5924564
netakiryosuke <sigoryosuke@gmail.com>
2025-10-13 17:44:41 +0900

    リザルト画面のメッセージを追加

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -48,7 +48,10 @@ export default function Result() {
                         <h5>一言：{getResultMessage(score)}</h5>
                     </>
                 ) : (
-                    <h2>質問に答えず、どのように相性を確かめましょうか？</h2>
+                    <>
+                        <h2>netakiryosukeとの相性一致度 -100％</h2>
+                        <h5>一言：質問に答えず相性が分かるならこんなアプリ作っていませんね。</h5>
+                    </>
                 )}
             </Box>
         </Box>
