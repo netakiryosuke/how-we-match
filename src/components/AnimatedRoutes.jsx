464dd45ce6a5bcb1e44269ccb5ff944a7b8d075a
netakiryosuke <sigoryosuke@gmail.com>
2025-10-13 20:03:27 +0900

    Animationのtransitionを修正

--- src/components/AnimatedRoutes.jsx
+++ src/components/AnimatedRoutes.jsx
@@ -17,7 +17,7 @@ export default function AnimatedRoutes() {
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
                             exit={{ opacity: 0 }}
-                            transition={{ duration: 2 }}
+                            transition={{ duration: 0.5 }}
                         >
                             <TopPage />
                         </motion.div>
