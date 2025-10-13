ee27f328375fc5bdae0311f792335df6f95d0a44
netakiryosuke <sigoryosuke@gmail.com>
2025-10-13 20:26:19 +0900

    葉っぱが画面下の見える範囲で消えないように修正

--- src/components/FallingLeaves.jsx
+++ src/components/FallingLeaves.jsx
@@ -37,7 +37,7 @@ export default function FallingLeaves({ imageSrc, count }) {
                         transform: `translateY(0) rotate(0deg)`
                     },
                     {
-                        transform: `translateY(100vh) rotate(${spin}deg)`,
+                        transform: `translateY(calc(100vh + 400px)) rotate(${spin}deg)`,
                     },
                 ],
                 {
