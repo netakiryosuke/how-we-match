fb99d4115b22e581978f1fb316cecc5baf27d95d
netakiryosuke <sigoryosuke@gmail.com>
2025-10-13 19:36:51 +0900

    navigationを実装

--- src/components/TopPage.jsx
+++ src/components/TopPage.jsx
@@ -1,10 +1,13 @@
 import { Box, Button, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 export default function TopPage() {
+    const navigate = useNavigate();
     return (
         <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", mt: 10, mb: 5 }}>
-            <Typography>あなたのことについて考えてください。</Typography>
-            <Button>診断を開始</Button>
+            <Typography variant="h2">無料相性診断テスト</Typography>
+            <Typography variant="h5">自分のこととして回答してください</Typography>
+            <Button onClick={() => navigate("/question")}>診断を開始</Button>
         </Box>
     );
 }
