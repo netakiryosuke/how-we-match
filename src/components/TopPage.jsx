ecb44614e8d9526ee3c9820894877cbc10fde9d7
netakiryosuke <sigoryosuke@gmail.com>
2025-10-13 19:46:09 +0900

    フォントを追加

--- src/components/TopPage.jsx
+++ src/components/TopPage.jsx
@@ -3,10 +3,10 @@ import { useNavigate } from "react-router-dom";
 
 export default function TopPage() {
     const navigate = useNavigate();
+
     return (
-        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", mt: 10, mb: 5 }}>
-            <Typography variant="h2">無料相性診断テスト</Typography>
-            <Typography variant="h5">自分のこととして回答してください</Typography>
+        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", mt: 10, mb: 5, gap: 8}}>
+            <Typography variant="h2" sx={{fontFamily: "'Noto Sans JP', sans-serif", fontSize: "54px"}}>無料相性診断テスト</Typography>
             <Button onClick={() => navigate("/question")}>診断を開始</Button>
         </Box>
     );
