7dc9cf4db903a35989899d733c95fb421996cbfd
netakiryosuke <sigoryosuke@gmail.com>
2025-10-13 19:59:43 +0900

    デザインの修正

--- src/components/TopPage.jsx
+++ src/components/TopPage.jsx
@@ -5,9 +5,25 @@ export default function TopPage() {
     const navigate = useNavigate();
 
     return (
-        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", mt: 10, mb: 5, gap: 8}}>
-            <Typography variant="h2" sx={{fontFamily: "'Noto Sans JP', sans-serif", fontSize: "54px"}}>無料相性診断テスト</Typography>
-            <Button onClick={() => navigate("/question")}>診断を開始</Button>
+        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", mt: 10, mb: 5, gap: 2}}>
+            <Typography variant="h2" sx={{ fontFamily: "'Noto Sans JP', sans-serif", fontSize: "54px", fontWeight: "bold" }}>無料相性診断テスト</Typography>
+            <Typography variant="h6" sx={{ fontFamily: "'Noto Sans JP', sans-serif"}}>あなたとnetakiryosukeの相性を診断します</Typography>
+            <Button
+                onClick={() => navigate("/question")}
+                sx={{
+                    width: "120px",
+                    height: "50px",
+                    fontSize: "16px",
+                    color: "white",
+                    borderRadius: "40px",
+                    backgroundColor: "#158bb9ff",
+                    borderColor: "#88619a",
+                    margin: "30px",
+                    ":hover": {
+                        opacity: 0.8
+                    }
+                }}
+            >診断を開始</Button>
         </Box>
     );
 }
