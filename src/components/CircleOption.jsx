e7ca878a07b50197c98ba78a2e5f261872bd42c6
netakiryosuke <sigoryosuke@gmail.com>
2025-10-15 21:20:30 +0900

    選択肢を選択時にチェックマークを表示

--- src/components/CircleOption.jsx
+++ src/components/CircleOption.jsx
@@ -1,11 +1,16 @@
 import { Box } from '@mui/material';
+import CheckIcon from '@mui/icons-material/Check';
 
 export default function CircleOption({ size, color, selected, onClick }) {
+    
 
     return (
         <Box
             onClick={onClick}
             sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
                 width: size,
                 height: size,
                 border: `3px solid ${color}`,
@@ -17,6 +22,13 @@ export default function CircleOption({ size, color, selected, onClick }) {
                 },
                 backgroundColor: selected ? color : "transparent"
             }}
-        />
+        >
+            {selected ? (
+                <CheckIcon sx={{
+                    color: "white",
+                    fontSize: size * 0.6,
+                }} />
+            ) : null}
+        </Box>
     );
 }
