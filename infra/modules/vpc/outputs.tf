d33d4c614c0eb46b99046cea6a16b3d9c1e42a54
netakiryosuke <sigoryosuke@gmail.com>
2025-12-21 23:46:10 +0900

    Fix VPC module outputs.tf

--- infra/modules/vpc/outputs.tf
+++ infra/modules/vpc/outputs.tf
@@ -2,14 +2,6 @@ output "vpc_id" {
   value = data.aws_vpc.main.id
 }
 
-output "subnet_pub_ids" {
-  value = data.aws_subnets.main_pub.ids
-}
-
-output "subnet_pro_ids" {
-  value = data.aws_subnets.main_pro.ids
-}
-
-output "subnet_pri_ids" {
-  value = data.aws_subnets.main_pri.ids
+output "subnet_ids" {
+  value = data.aws_subnets.main.ids
 }
