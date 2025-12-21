862ed7f55656e19c667d14c86ab7ec97ec60ac52
netakiryosuke <sigoryosuke@gmail.com>
2025-12-21 23:39:24 +0900

    Add A record

--- infra/modules/cloudfront/main.tf
+++ infra/modules/cloudfront/main.tf
@@ -59,6 +59,18 @@ resource "aws_route53_record" "acm" {
   records = [each.value.record]
 }
 
+resource "aws_route53_record" "site" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.main.domain_name
+    zone_id                = aws_cloudfront_distribution.main.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
 resource "aws_acm_certificate" "main" {
   provider          = aws.useast1
   domain_name       = var.domain_name
